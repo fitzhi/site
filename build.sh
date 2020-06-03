4fba7a1b771c1186b750c95313b66a94103724b7
frvidal <frederic.vidal.perso@gmail.com>
2020-06-03 08:23:32 +0200

    Update site

---
+++ build.sh
@@ -0,0 +1,7 @@
+cat header.html nav.html generated/fitzhi-backlog.html footer.html > backlog.html
+cat header.html nav.html generated/fitzhi-documentation.html footer.html > documentation.html
+mv generated/*.png .
+cat header.html nav.html generated/fitzhi-installation.html footer.html > installation.html
+cat header.html nav.html generated/fitzhi-documentation.html footer.html > documentation.html
+cat header.html nav.html generated/fitzhi-vision.html footer.html > vision.html
+
---
+++ generated/build-body.sh
@@ -0,0 +1,5 @@
+asciidoctor -r asciidoctor-diagram fitzhi-installation.adoc -e
+asciidoctor -r asciidoctor-diagram fitzhi-documentation.adoc -e
+asciidoctor -r asciidoctor-diagram fitzhi-vision.adoc -e
+asciidoctor -r asciidoctor-diagram fitzhi-backlog.adoc -e
+
