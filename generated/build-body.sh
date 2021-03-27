d611125cd54bf0d97e5692d40d7562d2b8f1ae85
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2021-03-27 20:28:11 +0100

    Update the Fitzhi website (less ASCIIDOC more HTML)

--- generated/build-body.sh
+++ generated/build-body.sh
@@ -1,5 +1,5 @@
 asciidoctor -r asciidoctor-diagram -b xhtml5 fitzhi-installation.adoc -e
 asciidoctor -r asciidoctor-diagram -b xhtml5 fitzhi-documentation.adoc -e
-asciidoctor -r asciidoctor-diagram -b xhtml5 fitzhi-vision.adoc -e
+# no more asciidoctor -r asciidoctor-diagram -b xhtml5 fitzhi-vision.adoc -e
 asciidoctor -b xhtml5 fitzhi-backlog.adoc -e
 asciidoctor -b xhtml5 fitzhi-contact.adoc -e
