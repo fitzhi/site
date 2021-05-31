c45aacf9cc2508ac30955f0fbaea805e1d4b3f27
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2021-05-31 18:50:52 +0200

    Update the installation page

--- build-html.sh
+++ build-html.sh
@@ -1,8 +1,8 @@
-cat header.html nav.html generated/fitzhi-backlog.html footer.html > backlog.html
-cat header.html nav.html generated/fitzhi-documentation.html footer.html > documentation.html
-mv generated/*.png .
+#cat header.html nav.html generated/fitzhi-backlog.html footer.html > backlog.html
+#cat header.html nav.html generated/fitzhi-documentation.html footer.html > documentation.html
+#mv generated/*.png .
 cat header.html nav.html generated/fitzhi-installation.html footer.html > installation.html
-cat header.html nav.html generated/fitzhi-documentation.html footer.html > documentation.html
-cat header.html nav.html generated/fitzhi-vision.html footer.html > vision.html
-cat header.html nav.html generated/fitzhi-contact.html footer.html > contact.html
+#cat header.html nav.html generated/fitzhi-documentation.html footer.html > documentation.html
+#cat header.html nav.html generated/fitzhi-vision.html footer.html > vision.html
+#cat header.html nav.html generated/fitzhi-contact.html footer.html > contact.html
 
