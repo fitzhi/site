98dfad9c533afaa8920adb0d84beee3cc96fe759
frvidal <frederic.vidal@fitzhi.com>
2021-11-02 20:05:22 +0100

    Update of website for fitzhi.com

--- build-html.sh
+++ build-html.sh
@@ -1,7 +1,10 @@
 #cat header.html nav.html generated/fitzhi-backlog.html footer.html > backlog.html
 #cat header.html nav.html generated/fitzhi-documentation.html footer.html > documentation.html
 #mv generated/*.png .
-cat header.html nav.html generated/fitzhi-installation.html footer.html > installation.html
+cat header.html fr/nav.html generated/fitzhi-installation.html footer.html > fr/installation.html
+cat header.html en/nav.html generated/fitzhi-installation.html footer.html > en/installation.html
+cp generated/main.png fr/main.png
+cp generated/main.png en/main.png
 #cat header.html nav.html generated/fitzhi-documentation.html footer.html > documentation.html
 #cat header.html nav.html generated/fitzhi-vision.html footer.html > vision.html
 #cat header.html nav.html generated/fitzhi-contact.html footer.html > contact.html
--- run_ad.sh
+++ run_ad.sh
@@ -1,1 +1,1 @@
-docker run -it -v /Users/frvidal/work/projects/site/generated:/documents/ asciidoctor/docker-asciidoctor
+docker run -it -v /Users/fredericvidal/work/projects/site/generated:/documents/ asciidoctor/docker-asciidoctor
