6497e55a48dfed8efec0b0f6e146bedfdabb2309
frvidal <frederic.vidal@fitzhi.com>
2021-11-26 17:55:05 +0100

    Adding the architecture & conffiguration pages

--- build-html.sh
+++ build-html.sh
@@ -5,6 +5,12 @@ cat header.html fr/nav.html generated/fitzhi-installation.html footer.html > fr/
 cat header.html en/nav.html generated/fitzhi-installation.html footer.html > en/installation.html
 cp generated/main.png fr/main.png
 cp generated/main.png en/main.png
+cat header.html fr/nav.html generated/fitzhi-getting-started.html footer.html > fr/getting-started.html
+cat header.html en/nav.html generated/fitzhi-getting-started.html footer.html > en/getting-started.html
+cat header.html fr/nav.html generated/fitzhi-configuration.html footer.html > fr/configuration.html
+cat header.html en/nav.html generated/fitzhi-configuration.html footer.html > en/configuration.html
+cat header.html fr/nav.html generated/fitzhi-architecture.html footer.html > fr/architecture.html
+cat header.html en/nav.html generated/fitzhi-architecture.html footer.html > en/architecture.html
 #cat header.html nav.html generated/fitzhi-documentation.html footer.html > documentation.html
 #cat header.html nav.html generated/fitzhi-vision.html footer.html > vision.html
 #cat header.html nav.html generated/fitzhi-contact.html footer.html > contact.html
--- generated/build-body.sh
+++ generated/build-body.sh
@@ -1,4 +1,7 @@
 asciidoctor -r asciidoctor-diagram -b xhtml5 fitzhi-installation.adoc -e
+asciidoctor -b xhtml5 fitzhi-getting-started.adoc -e
+asciidoctor -b xhtml5 fitzhi-configuration.adoc -e
+asciidoctor -r asciidoctor-diagram -b xhtml5 fitzhi-architecture.adoc -e
 # asciidoctor -r asciidoctor-diagram -b xhtml5 fitzhi-documentation.adoc -e
 # no more asciidoctor -r asciidoctor-diagram -b xhtml5 fitzhi-vision.adoc -e
 # asciidoctor -b xhtml5 fitzhi-backlog.adoc -e
